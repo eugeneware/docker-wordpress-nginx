dac7c13e534557d92a2d44aa264f87a570dd1fb6
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-05-12 17:07:48 -0400

    WordPress enables "pretty permalinks" by default as of 4.2.

--- start.sh
+++ start.sh
@@ -30,12 +30,11 @@ if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
   unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
   chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
 
-  # Activate nginx plugin and set up pretty permalink structure once logged in
+  # Activate nginx plugin once logged in
   cat << ENDL >> /usr/share/nginx/www/wp-config.php
 \$plugins = get_option( 'active_plugins' );
 if ( count( \$plugins ) === 0 ) {
   require_once(ABSPATH .'/wp-admin/includes/plugin.php');
-  \$wp_rewrite->set_permalink_structure( '/%postname%/' );
   \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
   foreach ( \$pluginsToActivate as \$plugin ) {
     if ( !in_array( \$plugin, \$plugins ) ) {
